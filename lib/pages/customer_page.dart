229523dc3cff6eac317d42c85102cf6c5029cb5d
ai_bat <baty0002@algonquinlive.com>
2025-07-30 23:42:19 -0400

    CustomerPage class created in customer_page.dart, initiated

--- lib/pages/customer_page.dart
+++ lib/pages/customer_page.dart
@@ -19,4 +19,45 @@ class CustomerPage extends StatefulWidget {
   State<CustomerPage> createState() => _CustomerPageState();
 }
 
+/// State class for CustomerPage with multi-language support
+///
+/// Manages language preference between English and French
+class _CustomerPageState extends State<CustomerPage> {
+  /// Current language setting - true for French, false for English
+  bool _isFrench = false;
+
+  /// List of customers loaded from database
+  List<Customer> _customers = [];
+
+  /// Currently selected customer for details view
+  Customer? _selectedCustomer;
+
+  /// Loading state indicator
+  bool _isLoading = true;
+
+  /// Form controllers for user input
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _addressController = TextEditingController();
+  final TextEditingController _dateOfBirthController = TextEditingController();
+
+  /// Database instance for data persistence
+  CustomerDatabase? _database;
+
+  /// Encrypted preferences for form data storage
+  EncryptedSharedPreferences? _encryptedPrefs;
+
+  /// Form mode: true for edit, false for add
+  bool _isEditMode = false;
+
+  /// Customer being edited (null for new customer)
+  Customer? _editingCustomer;
+
+  @override
+  void initState() {
+    super.initState();
+
+  }
+}
+
 
